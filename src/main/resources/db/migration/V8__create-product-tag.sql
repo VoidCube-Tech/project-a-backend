44e368d8b6dafcf43dd0ae4fc5146409d4693bce
Akir4 <dkaua0955@gmail.com>
2026-08-07 19:10:00 -0300

    Correção da migration V8

--- src/main/resources/db/migration/V8__create-product-tag.sql
+++ src/main/resources/db/migration/V8__create-product-tag.sql
@@ -4,12 +4,11 @@ CREATE TABLE product_tag(
     tenant_id BIGINT NOT NULL REFERENCES tenant(id)
 );
 
-CREATE UNIQUE INDEX ux_produc_tag_tenant_name
+CREATE UNIQUE INDEX ux_product_tag_tenant_name
 ON product_tag(tenant_id, LOWER(name));
 
-CREATE TABLE product_tag_association (
+CREATE TABLE product_tag_association(
     product_id BIGINT NOT NULL REFERENCES product(id),
-    tag_id BIGINT NOT NULL REFERENCES product_tag(id)
-
+    tag_id BIGINT NOT NULL REFERENCES product_tag(id),
     PRIMARY KEY(product_id, tag_id)
 );
